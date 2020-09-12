da0eaf070a09c6e8357c6c0d0fa9473bc4a0ac5c
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-09-12 10:00:10 +0200

    Replace by new React project
    Couldn't get the proxy working without webpack, so I dove into learning
    the basics of that after all.
    
    What I am not getting is why there are so many changes not staged for
    commit after I told git to add everything... Curious what my project looks
    like after commiting and pushing!

---
+++ services/client/src/components/App.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+export class App extends React.Component<any, any> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      recipe: "",
+    }
+  }
+
+  componentDidMount() {
+    fetch('grublub/webapi/myresource')
+      .then(response => response.text())
+      .then(recipe => this.setState({ recipe }));
+  }
+
+  public render(): JSX.Element {
+    console.log(this.state);
+
+    return <div>
+      <h1>{this.state.recipe}</h1>
+    </div>
+  }
+}
---
+++ services/client/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	entry: './src/index.tsx',
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "awesome-typescript-loader"
+			},
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader"
+			},
+			{
+				test: /\.scss$/,
+				use: [
+						 MiniCssExtractPlugin.loader,
+						 { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+						 { loader: 'sass-loader', options: { sourceMap: true } },
+				 ],
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./index.html"
+		}),
+		new MiniCssExtractPlugin({filename: "style.css"})
+	],
+	devtool: "source-map",
+	resolve: {
+		extensions: [".js", ".ts", ".tsx"]
+	},
+	devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			host: '0.0.0.0',
+			port: 3000,
+			disableHostCheck: true,
+			headers: {
+				'Cache-Control': 'no-cache, no-store',
+				'Expires': '-1',
+			},
+			proxy: {
+					'/grublub/*': 'http://localhost:8080/',
+			}
+	}
+}
