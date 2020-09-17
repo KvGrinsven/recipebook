d8136cd73fd4ada6e407753cc656e7812f209239
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-09-17 16:25:07 +0200

    Start form for adding recipe

--- services/client/src/components/AddPane.tsx
+++ services/client/src/components/AddPane.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export function AddPane() {
-  return <div><h1>New recipe</h1></div>
+
+  return <div className="AddPane">
+    <h1>New recipe</h1>
+    <form>
+      <label htmlFor="title">
+        Title: <input type="text" name="title" />
+      </label>
+      <br/>
+      <label htmlFor="servings">
+        Servings: <input type="number" name="servings" />
+      </label>
+      <p>... How do I want the ingredients to be entered?</p>
+      <label htmlFor="directions">
+        Directions: <input type="text" name="directions" />
+      </label>
+
+    </form>
+  </div>
 }
