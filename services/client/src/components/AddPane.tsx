7558fbaa01828091a1249008cd5e502501be316d
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-09-21 15:16:39 +0200

    Make (non-functioning) add form more demo-worthy :P

--- services/client/src/components/AddPane.tsx
+++ services/client/src/components/AddPane.tsx
@@ -5,18 +5,28 @@ export function AddPane() {
   return <div className="AddPane">
     <h1>New recipe</h1>
     <form>
-      <label htmlFor="title">
-        Title: <input type="text" name="title" />
-      </label>
-      <br/>
-      <label htmlFor="servings">
-        Servings: <input type="number" name="servings" />
-      </label>
-      <p>... How do I want the ingredients to be entered?</p>
-      <label htmlFor="directions">
-        Directions: <input type="text" name="directions" />
-      </label>
 
+      <label htmlFor="title">Title:</label><br/>
+      <input type="text" name="title"/><br/>
+
+      <label htmlFor="servings">Servings:</label><br/>
+      <input type="number" name="servings"/><br/>
+
+      <label htmlFor="ingredients">Ingredients:</label><br/>
+      <input list="ingredients" name="ingredients"/>
+      <datalist id="ingredients">
+        <option value="Potato"/>
+        <option value="Tomato"/>
+        <option value="Egg"/>
+        <option value="Flower"/>
+        <option value="Carrot"/>
+      </datalist><br/>
+
+      <label htmlFor="directions">Directions:</label><br/>
+      <input type="text" name="directions" /><br/>
+
+      <br/>
+      <input type="submit" value="Save"/>
     </form>
   </div>
 }
--- services/client/src/components/Ingredient.tsx
+++
@@ -1,5 +0,0 @@
-export interface Ingredient {
-    id: number;
-    description: string;
-    measure: string;
-}
